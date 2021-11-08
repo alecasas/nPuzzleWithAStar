dea7963c7d0906e7cfde17883bbb57f27a290224
jimenacotera <jimenacotera@github.com>
2021-11-08 10:41:22 -0500

    all should be working

--- main.py
+++ main.py
@@ -4,31 +4,34 @@ WEIGHT = 1.0
 
 
 class Node:
-    def __init__(self, data, level=0, f_value=0, prevAction=[]):
+    def __init__(self, data, level=0, f_value=0, prevAction = [] ):
         """ 
         Initialize the node with the data, level of the node and the calculated fvalue 
         """
         self.data = data  # might be array or 2d array
         self.level = level
         self.f_value = f_value
-        self.prevAction = prevAction
+        self.prevAction = prevAction #stores all moves in path in reverse order
+        self.prevValues = [] 
 
     def generate_child(self):
         """
         Generate all child nodes with possible moves.
+        return a list of all generated children
         """
-        x, y = self.find(self.data, '0')  # might have to change from char to int
+        x, y = self.find('0')  # might have to change from char to int
         pos_values = [[x, y - 1, "L"], [x, y + 1, "R"], [x - 1, y, "U"], [x + 1, y, "D"]]  # DULR
         children = []
         for i in pos_values:
             child_data = self.move_tile(self.data, x, y, i[0], i[1])
             if child_data is not None:  # if the move is possible
                 # child_node = Node(child_data, self.level + 1, 0, i[2])
-                child_node = Node(child_data, self.level + 1, 0)
-                # print(i[2] + "\n")
-                # child_node.lastMove += i[2]
+                child_node = Node(child_data, self.level + 1 , 0, [i[2]])
+                #print("i2 is : " + i[2])
                 children.append(child_node)
-            child_node.prevAction.append(i[2])
+        for ii in children: 
+            ii.prevAction += self.prevAction
+            ii.prevValues += self.prevValues
         return children
 
     def move_tile(self, puzzle_data, x1, y1, x2, y2):
@@ -46,14 +49,44 @@ class Node:
         else:
             return None
 
-    def find(self, puzzle_data, x):
+    def evalFunc(self, goal):
+        """
+        input: Goal node
+        Calculate f(n) = W*h(n) + g(n)
+        Assigns f(n) to self.f_value
+        writes f value to list to keep track
+        returns dereferenced f_value, -1 if failed
+        """
+        self.f_value = ( WEIGHT * self.manhattanDis(goal)) + self.level
+        self.prevValues += [self.f_value]
+        #return *(self.f_value)
+        return self.f_value
+
+    def manhattanDis(self, goal):
+        """
+        input: GOAL NODE
+        Calculates the different between the given puzzles
+        returns manhattan distance
+        """
+        dist = 0
+        for i in range(12): 
+            if i != 0:
+                currx, curry = self.find(str(i))
+                goalx, goaly = goal.find(str(i))
+                dist += abs(goalx - currx)
+                dist += abs(goaly - curry)
+        return dist
+
+    #def find(self, puzzle_data, x):
+    def find(self, x):
         """
         Returns the position int,int of where 0 is located
         """
         for i in range(0, len(self.data)):
             for j in range(0, len(self.data)):
-                if puzzle_data[i][j] == x:
+                if self.data[i][j] == x:
                     return i, j
+        return 100,100
 
     def copy(self, root):
         """
@@ -79,69 +112,47 @@ class Puzzle:
         self.reached = []
         self.nodes_generated = 1
         # self.moves = moves
-
-    def evalFunc(self, current, goal):
-        """
-        input: Node, Node
-        Calculate f(n) = W*h(n) + g(n)
-        """
-        return WEIGHT * self.manhattanDis(current.data, goal.data) + current.level
-
-    def manhattanDis(self, current, goal):
-        """
-        input: array, array ?
-        Calculates the different between the given puzzles
-        """
-        res = 0
-        for i in range(0, ROW):
-            for j in range(0, COL):
-                if current[i][j] != goal[i][j] and current[i][j] != 0:
-                    res += 1
-        return res
+        #self.moves = []
 
     def process(self, initialArray, goalArray):
         """
         input: Node, Node
         Accept Start and Goal Puzzle state
         """
-
         start = Node(initialArray)
         goal = Node(goalArray)
-        start.f_value = self.evalFunc(start, goal)
-
+        start.evalFunc(goal)
         """ Put the start node in the open list"""
         self.frontier.append(start)
-
-        print("\n\n")
+        #print("\n\n")
         while True:
             currState = self.frontier[0]
-            print("")
+            """ print("")
             print("  | ")
             print("  | ")
             print(" \\\'/ \n")
+            print(currState.level)
+            print(currState.f_value)
             for ii in currState.data:
                 for jj in ii:
                     print(jj, end=" ")
-                print("")
+                print("") """
             """ If the difference between current and goal node is 0 we have reached the goal node"""
-            if self.manhattanDis(currState.data, goal.data) == 0:
+            if currState.manhattanDis(goal) == 0:
                 self.writeOutput(start.data, currState)
                 break
             for ii in currState.generate_child():
-                ii.f_value = self.evalFunc(ii, goal)
+                ii.evalFunc(goal)
                 self.frontier.append(ii)
                 self.nodes_generated += 1
-
             self.reached.append(currState)
-
-
             del self.frontier[0]
-
             """ sort the open list based on f value """
             self.frontier.sort(key=lambda x: x.f_value, reverse=False)
 
     def writeOutput(self, start_data, currState):
         filename = input("Enter output file name: ")
+        filename = "file.txt"
         with open(filename, "w") as f:
             for ii in start_data:
                 temp = " ".join(ii)
@@ -160,11 +171,11 @@ class Puzzle:
             f.write(str(WEIGHT) + "\n")
             f.write((str(currState.level)) + "\n")
             f.write((str(self.nodes_generated)) + "\n")
-            # for move in self.moves:
-            #     f.write(move)
-            # f.write(currState.lastMove)
-            for action in currState.prevAction:
-                f.write(action+" ")
+            for ii in reversed(currState.prevAction):
+                f.write(str(ii) + " ")
+            f.write("\n")
+            for ii in reversed(currState.prevValues):
+                f.write(str(ii) + " ")
 
 
 def readFile(fname):  # to read the input and set up initial and goal state objects
@@ -185,19 +196,14 @@ def readFile(fname):  # to read the input and set up initial and goal state obje
 
 def main():
     # global WEIGHT
-    # f = input("Enter input file name: ")
+    f = input("Enter input file name: ")
     # f = "test_dude.txt"
-    f = 'Sample_Input.txt'
+    #f = 'Sample_Input.txt'
     # f = "correct_test.txt"
     initialArray, goalArray = readFile(f)
-    # WEIGHT = input("Please enter a valid weight: ")
+    WEIGHT = input("Please enter a valid weight: ")
     puzzle = Puzzle()
-    # puzzle.process()
     puzzle.process(initialArray, goalArray)
-    # print(initialState)
-    # print(goalState)
-    # gState, nodesGenerated = aStar(initialState, goalState)
-    # printOutput(gState, nodesGenerated)
 
 
 main()
